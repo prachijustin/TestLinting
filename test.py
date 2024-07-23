e17d3aa95ef95d3929018ce86a1318ae8cad9f98
Prachi Sharma <Prachi.Sharma@mottmac.com>
2024-07-23 11:17:24 +0530

    py file

--- test.py
+++ test.py
@@ -1,5 +1,31 @@
-print("hello world")
+print("hello world hello world hello world hello world hello world hello world hello world hello world hello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello world")
 
 a = 10
 
 print("byee...")
+
+def test_method(
+    a,
+    b,
+    c,
+    d,
+    e,
+    f,
+    g,
+    h,
+    i,
+    j,
+    k,
+    l,
+    m,
+    n,
+    o,
+    p,
+    q,
+    r,
+    s,
+    t,
+    u,
+    v
+):
+    pass
